4e3c122faf65890715e4e05beeac37d479625a01
TheAlchemistNerd <nevillemaloba@yahoo.com>
2022-07-27 14:13:46 +0300

    Successful ping of the bbc news rss feeds

---
+++ scraping.py
@@ -0,0 +1,27 @@
+from re import U
+from typing import List
+from regex import P
+import requests
+from bs4 import BeautifulSoup
+
+
+# scraping function
+def bbc_newsfeed_rss(_categories: List):
+    for _category in _categories:
+        if _category is 'sport':
+            url = f'https://feeds.bbci.co.uk/sport/rss.xml'
+        else:
+            url = f'http://feeds.bbci.co.uk/news/{_category}/rss.xml'
+        try:
+            r = requests.get(url)
+            print(f'The scraping job succeeded for {_category}: ', r.status_code)
+        except Exception as e:
+            print(f'The Scraping job failed for {_category}. See exception: ')
+            print(e)
+
+# different document categories for the BBC news RSS feeds
+categories = ['business','technology','science_and_environment','entertainment_and_arts','sport']
+
+print('Starting scraping')
+bbc_newsfeed_rss(categories)
+print('Finished scraping')
