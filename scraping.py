cbf9f75fa20c7237719342f4839305ba8cfca60e
TheAlchemistNerd <nevillemaloba@yahoo.com>
2022-07-27 15:00:13 +0300

    Parsing our data

--- scraping.py
+++ scraping.py
@@ -1,4 +1,3 @@
-from re import U
 from typing import List
 import requests
 from bs4 import BeautifulSoup
@@ -6,7 +5,9 @@ from bs4 import BeautifulSoup
 
 # scraping function
 def bbc_newsfeed_rss(_categories: List):
+    articles_dict = dict.fromkeys(_categories)
     for _category in _categories:
+        articles_dict[_category] = []
         if _category is 'sport':
             url = f'https://feeds.bbci.co.uk/sport/rss.xml'
         else:
@@ -14,7 +15,21 @@ def bbc_newsfeed_rss(_categories: List):
         try:
             r = requests.get(url)
             soup = BeautifulSoup(r.content, features = 'xml')
-            print(soup)
+            articles  = soup.findAll('item')
+            for a in articles:
+                title = a.find('title').text
+                link = a.find('link').text
+                published = a.find('pubDate').text
+                description = a.find('description').text
+                article = {
+                    'title':title,
+                    'link':link,
+                    'published': published,
+                    'description': description
+                }
+                articles_dict[_category].append(article)
+            
+            print(articles_dict)
         except Exception as e:
             print(f'The Scraping job failed for {_category}. See exception: ')
             print(e)
